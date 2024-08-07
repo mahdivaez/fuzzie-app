8a15ec8e4fe9b4b70237be98641885997d788c1e
Mehdi Vaez <mahdivaeztehrani81@gmail.com>
2024-08-07 07:03:03 -0700

    finish the navbar structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
-
+import Navbar from "@/components/global/navbar";
 export default function Home() {
   return (
     <main className="">
      <Navbar/>
+     <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+<div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
+
+
+  
+</div>
+</section>
     </main>
   );
 }
